e1100c5385de08ea2774097d3c83ae6881bed725
akshaypatel99 <akshaypatel99@gmail.com>
2024-03-08 19:54:40 +0000

    Refactor code

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { WeatherProvider } from './context/WeatherContext';
-import { initialState, weatherReducer } from './context/weatherReducer';
+import {
+	initialState,
+	weatherReducer,
+} from './context/weatherReducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<WeatherProvider initialState={initialState} reducer={weatherReducer}>
+			<WeatherProvider
+				initialState={initialState}
+				reducer={weatherReducer}>
 				<GlobalStyle />
 				<App />
 			</WeatherProvider>
